2e33537b2541f3db1e9b6c9822ab4f564e4d7ea8
Alex Rokachev <alserokachev@gmail.com>
2024-01-13 21:16:06 +0300

    added gonum lib

--- main.go
+++ main.go
@@ -2,27 +2,33 @@ package main
 
 import (
 	"fmt"
-	"gorgonia.org/tensor"
+	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
-	inputs := tensor.New(tensor.WithBacking([]float32{1, 2, 3, 2.5}))
-	weights := tensor.New(tensor.WithShape(3, 4), tensor.WithBacking([]float32{
+	rawInputs := []float64{
+		1, 2, 3, 2.5,
+		2, 5, -1, 2,
+		-1.5, 2.7, 3.3, -0.8,
+	}
+	rawWeights := []float64{
 		0.2, 0.8, -0.5, 1,
 		0.5, -0.91, 0.26, -0.5,
 		-0.26, -0.27, 0.17, 0.87,
-	}))
-	biases := tensor.New(tensor.WithBacking([]float32{2, 3, 0.5}))
-
-	dp, err := tensor.Dot(weights, inputs)
-	if err != nil {
-		panic("failed to find dot product: " + err.Error())
 	}
 
-	output, err := tensor.Add(dp, biases)
-	if err != nil {
-		panic("failed to add: " + err.Error())
+	biases := mat.NewDense(1, 3, []float64{2, 3, 0.5})
+	inputs := mat.NewDense(3, 4, rawInputs)
+	weights := mat.NewDense(3, 4, rawWeights)
+
+	weights.CloneFrom(weights.T())
+
+	output := mat.NewDense(3, 3, nil)
+	for j := 0; j < 3; j++ {
+		for i := 0; i < 3; i++ {
+			output.Set(i, j, mat.Dot(inputs.RowView(j), weights.ColView(i))+biases.At(0, i))
+		}
 	}
 
-	fmt.Println(dp, output)
+	fmt.Println(output)
 }
